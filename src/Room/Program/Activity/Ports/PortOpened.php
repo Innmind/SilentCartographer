bcfb6cf6883f07a5be8f419c6db778c8adbc3513
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-28 21:30:29 +0100

    add ports activity

---
+++ src/Room/Program/Activity/Ports/PortOpened.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\SilentCartographer\Room\Program\Activity\Ports;
+
+use Innmind\SilentCartographer\Room\Program\{
+    Activity,
+    Activity\Tags,
+};
+use Innmind\Socket\Internet\Transport;
+use Innmind\IP\IP;
+use Innmind\Url\Authority\PortInterface;
+
+final class PortOpened implements Activity
+{
+    private $transport;
+    private $ip;
+    private $port;
+    private $tags;
+
+    public function __construct(
+        Transport $transport,
+        IP $ip,
+        PortInterface $port
+    ) {
+        $this->transport = $transport;
+        $this->ip = $ip;
+        $this->port = $port;
+        $this->tags = new Tags('os', 'socket', 'port');
+    }
+
+    public function tags(): Tags
+    {
+        return $this->tags;
+    }
+
+    public function __toString(): string
+    {
+        return "Port opened: {$this->transport}://{$this->ip}:{$this->port}";
+    }
+}
