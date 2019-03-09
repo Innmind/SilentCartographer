1e0f8b20e4f54071f811cba26bfcd5f0bd7a13f9
Baptiste Langlade <baptouuuu@gmail.com>
2019-03-09 10:17:58 +0100

    add command to automatically start the sub routine when another command is run

---
+++ tests/Command/AutoStartSubRoutineTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\SilentCartographer\Command;
+
+use Innmind\SilentCartographer\Command\AutoStartSubRoutine;
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Server\Control\Server\Processes;
+use Innmind\Url\Path;
+use PHPUnit\Framework\TestCase;
+
+class AutoStartSubRoutineTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new AutoStartSubRoutine(
+                $this->createMock(Command::class),
+                $this->createMock(Processes::class)
+            )
+        );
+    }
+
+    public function testUsage()
+    {
+        $command = new AutoStartSubRoutine(
+            $inner = $this->createMock(Command::class),
+            $this->createMock(Processes::class)
+        );
+        $inner
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn('foo');
+
+        $this->assertSame('foo', (string) $command);
+    }
+
+    public function testInvokation()
+    {
+        $command = new AutoStartSubRoutine(
+            $inner = $this->createMock(Command::class),
+            $processes = $this->createMock(Processes::class)
+        );
+        $processes
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->callback(static function($command): bool {
+                return (string) $command === "silent-cartographer 'sub-routine'" &&
+                    $command->workingDirectory() === '/somewhere';
+            }));
+        $env = $this->createMock(Environment::class);
+        $arguments = new Arguments;
+        $options = new Options;
+        $env
+            ->expects($this->once())
+            ->method('workingDirectory')
+            ->willReturn(new Path('/somewhere'));
+        $inner
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($env, $arguments, $options);
+
+        $this->assertNull($command($env, $arguments, $options));
+    }
+}
